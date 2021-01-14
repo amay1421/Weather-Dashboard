0e6e458ce61f292859b0729e36ef7faed3e4261d
Alyson May <amay1421@yahoo.com>
2021-01-13 20:54:04 -0600

    current weather dashboard

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,6 @@
 var cityName = $("#search-city").val();
 
-var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=0d2a7f7bab7c452a6b8b48c2632901db";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=0d2a7f7bab7c452a6b8b48c2632901db";
 
 
 // // $.ajax({
@@ -14,15 +14,34 @@ var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/
 $("#search-button").on("click", function(event) {
     event.preventDefault();
     var cityName = $("#search-city").val().trim();
-    var queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=0d2a7f7bab7c452a6b8b48c2632901db";
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=0d2a7f7bab7c452a6b8b48c2632901db";
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function (response) {
+          console.log(response)
+        //   Get Current city name
+          var currentCity = $("#name")
+          currentCity.text(response.name)
+          $("#name").append(currentCity)
+            // Get Current City Temp
+          var Kalvin = response.main.temp
+          var temperature = (Kalvin -273.15) * 1.80 + 32
+          tempF = Math.floor(temperature);
+          $("#temperature").text(tempF + " F")   
+          // Get Current Humidity
+          var currentHumidity = $("#humidity")
+          currentHumidity.text(response.main.humidity + "%")
+          $("#humidity").append(currentHumidity)
+          // Get Wind Speed
+          var currentWindspeed = $("#wind-speed")
+          currentWindspeed.text(response.wind.speed + " MPH")
+          $("#wind-speed").append(currentWindspeed)
+          // Get UV index
 
-    });
+    // "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
-//     // getWeather(cityName);
+    })
   });
 
 
@@ -37,4 +56,3 @@ $("#search-button").on("click", function(event) {
 // });
 
 
-// let tempF =(response.main.temp -273.15) * 1.80 + 32;
